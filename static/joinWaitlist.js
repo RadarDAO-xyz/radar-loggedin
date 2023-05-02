b1eda52aa873f9e5427e21f9986e43a6077fb0f8
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-05-02 15:30:26 +0300

    fix waitingFor

--- static/joinWaitlist.js
+++ static/joinWaitlist.js
@@ -1,27 +1,34 @@
 const API = 'https://api.radardao.xyz';
 
 let submitted = false;
-$('#email-form').submit(async e => {
-    e.preventDefault();
-    if (submitted) return;
-    submitted = true;
-    const headers = new Headers();
-    headers.set('Content-Type', 'application/json');
-    const res = await fetch(`${API}/joinWaitlist`, {
-        headers,
-        method: 'POST',
-        body: JSON.stringify({
-            email: $('#email-textbox').val(),
-            waitingFor: $('#waitingfor').text()
+if (waitingForName) {
+    $('#email-form').submit(async e => {
+        e.preventDefault();
+        if (submitted) return;
+
+        if (!$('#email-textbox').val()) {
+            return $('#submit-button').text('Email is required');
+        }
+
+        submitted = true;
+        const headers = new Headers();
+        headers.set('Content-Type', 'application/json');
+        const res = await fetch(`${API}/joinWaitlist`, {
+            headers,
+            method: 'POST',
+            body: JSON.stringify({
+                email: $('#email-textbox').val(),
+                waitingFor: waitingForName
+            })
         })
-    })
-        .then(r => r.json())
-        .catch(() => {});
-    $('#email-form').hide();
-    if (res?.waitlisted) {
-        $('.success-message.w-form-done').show();
-    } else {
-        $('.error-message.w-form-fail').show();
-    }
-    console.log(res);
-});
+            .then(r => r.json())
+            .catch(() => {});
+        $('#email-form').hide();
+        if (res?.waitlisted) {
+            $('.success-message.w-form-done').show();
+        } else {
+            $('.error-message.w-form-fail').show();
+        }
+        console.log(res);
+    });
+}
