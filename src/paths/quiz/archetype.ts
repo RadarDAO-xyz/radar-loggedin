15900769a55cd198aeedb328405551f25f88c3cb
mrTomatolegit <58458857+mrTomatolegit@users.noreply.github.com>
2023-05-06 19:46:08 +0300

    comment out archetype

--- src/paths/quiz/archetype.ts
+++ src/paths/quiz/archetype.ts
@@ -24,6 +24,11 @@ const answers = ['a', 'b', 'c', 'd', 'e'] as (keyof AnswerFrequencies)[];
 
 const between = (l: number, n: number | undefined, h: number) => (n ? n >= l && n <= h : false);
 
+/**
+ * This determines the Archetype based on answer frequencies
+ * @param percentages
+ * @returns
+ */
 function determineArchetype(percentages: AnswerFrequencies) {
     for (let i = 0; i < 5; i++) {
         const id = pures[i];
@@ -31,14 +36,14 @@ function determineArchetype(percentages: AnswerFrequencies) {
         const percentage = percentages[answer];
         const percentageNext = (x: number) => percentages[answers[i + x]];
 
-        if (percentage >= 80) return id;
-        if (between(40, percentage, 50) && between(40, percentageNext(1), 70)) return id + 10;
-        if (between(40, percentage, 50) && between(40, percentageNext(2), 70)) return id + 14;
-        if (between(40, percentage, 50) && between(40, percentageNext(3), 70)) return id + 17;
-        if (between(40, percentage, 50) && between(40, percentageNext(4), 70)) return id + 19;
-        if (between(40, percentage, 70)) return id + 5;
+        if (percentage >= 80) return id; // 8+ X is pure
+        if (between(40, percentage, 50) && between(40, percentageNext(1), 70)) return id + 10; // 4-5 X and 4-5 Y1 is XY1
+        if (between(40, percentage, 50) && between(40, percentageNext(2), 70)) return id + 14; // 4-5 X and 4-5 Y2 is XY2
+        if (between(40, percentage, 50) && between(40, percentageNext(3), 70)) return id + 17; // 4-5 X and 4-5 Y3 is XY3
+        if (between(40, percentage, 50) && between(40, percentageNext(4), 70)) return id + 19; // 4-5 X and 4-5 Y4 is XY4
+        if (between(40, percentage, 70)) return id + 5; // 4-7 X is X mix
     }
-    return 21;
+    return 21; // All answer frequencies are 3 or <3
 }
 
 ArchetypeRouter.get('/', async (req, res) => {
